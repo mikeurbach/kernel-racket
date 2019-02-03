3d5d1fa44a740ded4d5a08a4e7845afbfe0e2b14
Mike Urbach <murbach@teambanjo.com>
2019-02-02 17:14:02 -0800

    leave not about |#ignore| in kernel-symbol?

--- kernel/core/symbol.rkt
+++ kernel/core/symbol.rkt
@@ -2,6 +2,7 @@
 
 (provide kernel-symbol?)
 
+;; TODO: maybe don't use |#ignore| symbol directly in the evaluator logic
 (define (kernel-symbol? object)
   (and (not (eqv? object '|#ignore|))
        (symbol? object)))
