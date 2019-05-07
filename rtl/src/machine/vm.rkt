aa4a67334c5aeff9b88a7b86d212bdf424ca801a
Mike Urbach <murbach@teambanjo.com>
2019-05-06 19:34:11 -0700

    add ability to assign register from operator result

--- rtl/src/machine/vm.rkt
+++ rtl/src/machine/vm.rkt
@@ -8,14 +8,27 @@
 
     (define (extract-registers insts)
       (if (empty? insts)
-          (hasheq)
+          (hash)
           (let ([inst-registers (extract-inst-registers (car insts))])
-            (hash-union inst-registers (extract-registers (cdr insts))))))
+            (hash-union inst-registers (extract-registers (cdr insts))
+                        #:combine/key (lambda (k v1 v2) v2)))))
 
     (define (extract-inst-registers instruction)
       (match instruction
-        [(list 'assign name _) (hasheq name (register #f))]
-        [_ (hasheq)]))
+        [(list 'assign name _ ...) (hash name (register #f))]
+        [_ (hash)]))
+
+    (define (extract-operators insts)
+      (if (empty? insts)
+          (hash)
+          (let ([inst-operators (extract-inst-operators (car insts))])
+            (hash-union inst-operators (extract-operators (cdr insts))
+                        #:combine/key (lambda (k v1 v2) v2)))))
+
+    (define (extract-inst-operators instruction)
+      (match instruction
+        [(list 'assign _ (list 'op operator) _ ...) (hash operator (eval operator))]
+        [_ (hash)]))
 
     (define (extract-execution-procs insts)
       (map extract-execution-proc insts))
@@ -23,7 +36,8 @@
     (define (extract-execution-proc instruction)
       (match instruction
         [(list 'assign name (list 'const value)) (make-assign-const name value)]
-        [(list 'assign name (list 'reg arg-name)) (make-assign-reg name arg-name)]))
+        [(list 'assign name (list 'reg arg-name)) (make-assign-reg name arg-name)]
+        [(list 'assign name (list 'op operator) inputs ...) (make-assign-op name operator inputs)]))
 
     (define (make-assign-const name value)
       (lambda ()
@@ -34,15 +48,38 @@
     (define (make-assign-reg name arg-name)
       (lambda ()
         (let ([register (hash-ref registers name)]
-              [arg-register (hash-ref registers arg-name)])
-          (set-register-value! register (register-value arg-register))
+              [arg-exp (make-register-exp arg-name)])
+          (set-register-value! register (arg-exp))
           (advance-pc!))))
+
+    (define (make-assign-op name operator inputs)
+      (let ([operator-proc (hash-ref operators operator)]
+            [register (hash-ref registers name)]
+            [arg-exps (map (lambda (e) (make-primitive-exp e)) inputs)])
+        (lambda ()
+          (let ([args (map (lambda (e) (e)) arg-exps)])
+            (set-register-value! register (apply operator-proc args))
+            (advance-pc!)))))
+
+    (define (make-primitive-exp expression)
+      (match expression
+        [(list 'const value) (make-const-exp value)]
+        [(list 'reg name) (make-register-exp name)]))
+
+    (define (make-const-exp value)
+      (lambda () value))
+
+    (define (make-register-exp name)
+      (lambda ()
+        (let ([register (hash-ref registers name)])
+          (register-value register))))
 
     (define (advance-pc!)
       (let ([new-pc (cdr (register-value pc))])
         (set-register-value! pc new-pc)))
 
     (define registers (extract-registers instructions))
+    (define operators (extract-operators instructions))
     (define program (extract-execution-procs instructions))
     (define pc (register program))
 
@@ -56,5 +93,8 @@
 
     (define/public (get-registers)
       registers)
+
+    (define/public (get-operators)
+      operators)
 
     (super-new)))
