dc5cbce3f6c60592002d4d704aea0c706c91f4d9
Mike Urbach <murbach@teambanjo.com>
2019-05-15 10:32:40 -0700

    add branches with both operator predicates and #t default

--- rtl/src/machine/vm.rkt
+++ rtl/src/machine/vm.rkt
@@ -22,6 +22,8 @@
       (match instruction
         [(list 'assign _ (list 'op operator) _ ...)
          (hash operator (eval operator))]
+        [(list 'branch (list (list (list 'op operator) _ ...) _ ...) _ ...)
+         (hash operator (eval operator))]
         [_ (hash)]))
 
     (define (extract-execution-procs insts)
@@ -35,7 +37,9 @@
         [(list 'assign name (list 'reg arg-name))
          (make-assign-reg name arg-name)]
         [(list 'assign name (list 'op operator) inputs ...)
-         (make-assign-op name operator inputs)]))
+         (make-assign-op name operator inputs)]
+        [(list 'branch (list predicates labels) ...)
+         (make-branch predicates labels)]))
 
     (define (extract-basic-blocks insts)
       (if (empty? insts)
@@ -61,11 +65,40 @@
     (define (make-assign-op name operator inputs)
       (let ([operator-proc (hash-ref operators operator)]
             [register (hash-ref registers name)]
-            [arg-exps (map (lambda (e) (make-primitive-exp e)) inputs)])
+            [arg-exps (map make-primitive-exp inputs)])
         (lambda ()
           (let ([args (map (lambda (e) (e)) arg-exps)])
             (set-register-value! register (apply operator-proc args))
             (advance-pc!)))))
+
+    (define (make-branch predicates labels)
+      (let ([predicate-procs (make-branch-predicates predicates)])
+        (lambda ()
+          (test-branch-predicates predicate-procs labels))))
+
+    (define (make-branch-predicates predicates)
+      (if (empty? predicates)
+          '()
+          (cons (make-branch-predicate (car predicates)) (cdr predicates))))
+
+    (define (make-branch-predicate predicate)
+      (match predicate
+        [(list (list 'op operator) inputs ...)
+         (let ([operator-proc (hash-ref operators operator)]
+               [arg-exps (map make-primitive-exp inputs)])
+           (lambda ()
+             (let ([args (map (lambda (e) (e)) arg-exps)])
+               (apply operator-proc args))))]
+        [#t (lambda () #t)]))
+
+    (define (test-branch-predicates predicate-procs labels)
+      (if (empty? predicate-procs)
+          (advance-pc!)
+          (let ([predicate-proc (car predicate-procs)]
+                [label (car labels)])
+            (if (predicate-proc)
+                (set-pc! label)
+                (test-branch-predicates (cdr predicate-procs) (cdr labels))))))
 
     (define (make-primitive-exp expression)
       (match expression
@@ -84,6 +117,10 @@
       (let ([new-pc (cdr (register-value pc))])
         (set-register-value! pc new-pc)))
 
+    (define (set-pc! label)
+      (let ([new-pc (hash-ref basic-blocks label)])
+        (set-register-value! pc new-pc)))
+
     (define registers
       (traverse-instructions extract-inst-registers instructions))
 
@@ -94,7 +131,7 @@
 
     (define basic-blocks (extract-basic-blocks execution-procs))
 
-    (define program (filter (lambda (p) (not (symbol? p))) execution-procs))
+    (define program (filter (compose1 not symbol?) execution-procs))
 
     (define pc (register program))
 
