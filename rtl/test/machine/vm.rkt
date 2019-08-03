fb299103bcdf3e5336c963466ed53a169e385c20
mikeurbach <mikeurbach@gmail.com>
2019-08-03 14:54:43 -0500

    smooth register target syntax wrt operand, and add alias support

--- rtl/test/machine/vm.rkt
+++ rtl/test/machine/vm.rkt
@@ -23,101 +23,122 @@
   (let ([register-value (send myvm vm-get-register-value name)])
     (check-equal? register-value value)))
 
-(let ([myvm (run-vm '((assign (foo (const 420)))))])
+(let ([myvm (run-vm '((assign ((reg foo) (const 420)))))])
   (assert-register myvm 'foo 420))
 
-(let ([myvm (run-vm '((assign (foo (const 420)))
-                      (assign (foo (const 69)))))])
+(let ([myvm (run-vm '((assign ((reg foo) (const 420)))
+                      (assign ((reg foo) (const 69)))))])
   (assert-register myvm 'foo 69))
 
-(let ([myvm (run-vm '((assign (foo (const 420)))
-                      (assign (bar (reg foo)))))])
+(let ([myvm (run-vm '((assign ((reg foo) (const 420)))
+                      (assign ((reg bar) (reg foo)))))])
   (assert-register myvm 'foo 420)
   (assert-register myvm 'bar 420))
 
-(let ([myvm (run-vm '((assign (foo (const 420)))
-                      (assign (bar (const 69)))
-                      (assign (baz (op +) (reg foo) (reg bar)))))])
+(let ([myvm (run-vm '((assign ((reg foo) (const 420)))
+                      (assign ((reg bar) (const 69)))
+                      (assign ((reg baz) (op +) (reg foo) (reg bar)))))])
   (assert-register myvm 'foo 420)
   (assert-register myvm 'bar 69)
   (assert-register myvm 'baz 489))
 
-(let ([myvm (run-vm '((assign (val (const 1)))
+(let ([myvm (run-vm '((assign ((reg val) (const 1)))
                       (branch (#t (const done)))
-                      (assign (val (const 2)))
+                      (assign ((reg val) (const 2)))
                       done))])
   (assert-register myvm 'val 1))
 
-(let ([myvm (run-vm '((assign (val (const 1)))
+(let ([myvm (run-vm '((assign ((reg val) (const 1)))
                       loop
-                      (assign (val (op +) (reg val) (const 1)))
+                      (assign ((reg val) (op +) (reg val) (const 1)))
                       (branch (((op <) (reg val) (const 10)) (const loop)))))])
   (assert-register myvm 'val 10))
 
-(let ([myvm (run-vm '((assign (val (const 1)))
+(let ([myvm (run-vm '((assign ((reg val) (const 1)))
                       loop
-                      (assign (val (op +) (reg val) (const 1)))
+                      (assign ((reg val) (op +) (reg val) (const 1)))
                       (branch (((op eq?) (reg val) (const 5)) (const done))
                               (((op <) (reg val) (const 10)) (const loop)))
                       done))])
   (assert-register myvm 'val 5))
 
-(let ([myvm (run-vm '((assign (a (const 1)) (b (reg a)))))])
+(let ([myvm (run-vm '((assign ((reg a) (const 1))
+                              ((reg b) (reg a)))))])
   (assert-register myvm 'a 1)
   (assert-register myvm 'b #f))
 
-(let ([myvm (run-vm '((assign (a (const 420)) (b (const 69)))
-                      (assign (a (reg b)) (b (reg a)))))])
+(let ([myvm (run-vm '((assign ((reg a) (const 420))
+                              ((reg b) (const 69)))
+                      (assign ((reg a) (reg b))
+                              ((reg b) (reg a)))))])
   (assert-register myvm 'a 69)
   (assert-register myvm 'b 420))
 
 ;; branch from register
-(let ([myvm (run-vm '((assign (label-reg (const a)))
+(let ([myvm (run-vm '((assign ((reg label-reg) (const a)))
                       (branch (#t (reg label-reg)))
-                      (assign (val (const wrong)))
+                      (assign ((reg val) (const wrong)))
                       (branch (#t (const done)))
                       a
-                      (assign (val (const right)))
+                      (assign ((reg val) (const right)))
                       done))])
   (assert-register myvm 'val 'right))
 
+;; register aliasing for read
+(let ([myvm (run-vm '((assign ((reg foo) (const 1))
+                              ((reg myalias) (const foo)))
+                      (assign ((reg result) (alias myalias)))))])
+  (assert-register myvm 'result 1))
+
+;; register aliasing for write
+(let ([myvm (run-vm '((assign ((reg foo) (const 1))
+                              ((reg myalias) (const foo)))
+                      (assign ((alias myalias) (const 2)))))])
+  (assert-register myvm 'foo 2))
+
 ;; GCD
-(let ([myvm (new vm [environment test-environment] [instructions '(test-b
-                                    (branch (((op eq?) (reg b) (const 0)) (const done)))
-                                    (assign (a (reg b))
-                                            (b (op remainder) (reg a) (reg b)))
-                                    (branch (#t (const test-b)))
-                                    done
-                                    (assign (result (reg a))))])])
+(let ([myvm (new vm
+                 [environment test-environment]
+                 [instructions '(test-b
+                                 (branch (((op eq?) (reg b) (const 0)) (const done)))
+                                 (assign ((reg a) (reg b))
+                                         ((reg b) (op remainder) (reg a) (reg b)))
+                                 (branch (#t (const test-b)))
+                                 done
+                                 (assign ((reg result) (reg a))))])])
   (send myvm vm-set-register-value! 'a 24)
   (send myvm vm-set-register-value! 'b 9)
   (send myvm execute)
   (assert-register myvm 'result 3))
 
 ;; Fibonacci
-(let ([myvm (new vm [environment test-environment] [instructions '((assign (prev (const 0))
-                                            (curr (const 1)))
-                                    test-n
-                                    (branch (((op eq?) (reg n) (const 0)) (const done)))
-                                    (assign (curr (op +) (reg prev) (reg curr))
-                                            (prev (reg curr))
-                                            (n (op -) (reg n) (const 1)))
-                                    (branch (#t (const test-n)))
-                                    done
-                                    (assign (result (reg prev))))])])
+(let ([myvm (new vm
+                 [environment test-environment]
+                 [instructions '((assign ((reg prev) (const 0))
+                                         ((reg curr) (const 1)))
+                                 test-n
+                                 (branch (((op eq?) (reg n) (const 0)) (const done)))
+                                 (assign ((reg curr) (op +) (reg prev) (reg curr))
+                                         ((reg prev) (reg curr))
+                                         ((reg n) (op -) (reg n) (const 1)))
+                                 (branch (#t (const test-n)))
+                                 done
+                                 (assign ((reg result) (reg prev))))])])
   (send myvm vm-set-register-value! 'n 100)
   (send myvm execute)
   (assert-register myvm 'result 354224848179261915075))
 
 ;; Factorial
-(let ([myvm (new vm [environment test-environment] [instructions '((assign (product (const 1)))
-                                    test-n
-                                    (branch (((op eq?) (reg n) (const 0)) (const done)))
-                                    (assign (product (op *) (reg n) (reg product))
-                                            (n (op -) (reg n) (const 1)))
-                                    (branch (#t (const test-n)))
-                                    done
-                                    (assign (result (reg product))))])])
+(let ([myvm (new vm
+                 [environment test-environment]
+                 [instructions '((assign ((reg product) (const 1)))
+                                 test-n
+                                 (branch (((op eq?) (reg n) (const 0)) (const done)))
+                                 (assign ((reg product) (op *) (reg n) (reg product))
+                                         ((reg n) (op -) (reg n) (const 1)))
+                                 (branch (#t (const test-n)))
+                                 done
+                                 (assign ((reg result) (reg product))))])])
   (send myvm vm-set-register-value! 'n 5)
   (send myvm execute)
   (assert-register myvm 'result 120))
