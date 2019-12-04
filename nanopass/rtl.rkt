15e14a3ec44ab8a7dd4a03edeab8d77856016e90
mikeurbach <mikeurbach@gmail.com>
2019-12-04 15:49:24 -0800

    add language that is closer to FSM description, and a pass for it

--- nanopass/rtl.rkt
+++ nanopass/rtl.rkt
@@ -1,6 +1,6 @@
 #lang nanopass
 
-(provide rtl-adt output-rtl)
+(provide rtl-adt adt-to-fsm)
 
 (define (size? e)
   (and (pair? e)
@@ -100,79 +100,98 @@
   (Module (module)
     (symbol (declaration ...) (operation ...))))
 
-(define-pass output-rtl : rtl-adt (ast) -> * ()
-  (register-pass : Register (r) -> * ()
-    [(reg ,symbol) (list 'reg symbol)]
-    [(reg ,symbol ,size) (list 'reg symbol size)])
-  (constant-pass : Constant (c) -> * ()
-    [(const ,bitwidth ,baseident ,literal) (list bitwidth baseident literal)])
-  (input-pass : Input (i) -> * ()
-    [(in ,symbol) (list 'in symbol)]
-    [(in ,symbol ,size) (list 'in symbol size)])
-  (output-pass : Output (i) -> * ()
-    [(out ,symbol) (list 'out symbol)]
-    [(out ,symbol ,size) (list 'out symbol size)])
-  (port-pass : Port (p) -> * ())
-  (memory-ref-pass : MemoryRef (mr) -> * ())
-  (memory-pass : Memory (m) -> * ()
-    [(mem ,symbol ,[memory-ref-pass : memory-ref]) (list 'mem symbol memory-ref)])
-  (memory-decl-pass : MemoryDecl (md) -> * ()
-    [(mem ,symbol ,size0 ,size1) (list 'mem symbol size0 size1)])
-  (declaration-pass : Declaration (d) -> * ())
-  (target-pass : Target (t) -> * ())
-  (value-pass : Value (v) -> * ())
-  (unary-op-pass : UnaryOp (uo) -> * ()
-    [(op ,unop) (list 'op unop)])
-  (binary-op-pass : BinaryOp (bo) -> * ()
-    [(op ,binop) (list 'op binop)])
-  (assign-pass : Assign (a) -> * ()
-    [(,[target-pass : target] ,[value-pass : value])
-     (list target value)]
-    [(,[target-pass : target] ,[unary-op-pass : unary-op] ,[value-pass : value])
-     (list target unary-op value)]
-    [(,[target-pass : target] ,[value-pass : value1] ,[binary-op-pass : binary-op] ,[value-pass : value2])
-     (list target value1 binary-op value2)])
-  (case-statement-pass : CaseStatement (cs) -> * ()
-    [(case ,[value-pass : value0] (,[value-pass : value1] ,symbol1) ... ,symbol0)
-     (list 'case value0 (map cons value1 symbol1) symbol0)])
-  (next-state-pass : NextState (ns) -> * ()
-    [,symbol symbol])
+(define-language rtl-fsm
+  (extends rtl-adt)
+  (AssignState (assign-state)
+    (+ (symbol (assign ...))))
+  (NextStateState (next-state-state)
+    (+ (symbol next-state)))
+  (Module (module)
+    (- (symbol (declaration ...) (operation ...)))
+    (+ (symbol0 (symbol1 ...) (port ...) (declaration ...) (assign-state ...) (next-state-state ...)))))
+
+(define-pass adt-to-fsm : rtl-adt (ast) -> rtl-fsm ()
+  (definitions
+    (define (extract-operation-names operations)
+      (for/list ([operation operations])
+        (car operation)))
+    (define (extract-ports operations)
+      (remove-duplicates
+       (flatten
+        (map cadr operations))))
+    (define (extract-assign-states operations)
+      (flatten
+       (for/list ([operation operations])
+         (for/list ([state (caddr operation)])
+           (let ([label (car state)]
+                 [assigns (cadr state)])
+             (with-output-language (rtl-fsm AssignState)
+               `(,label (,assigns ...))))))))
+    (define (extract-next-states operations)
+      (flatten
+       (for/list ([operation operations])
+         (for/list ([state (caddr operation)])
+           (let ([label (car state)]
+                 [next-state (caddr state)])
+             (with-output-language (rtl-fsm NextStateState)
+               `(,label ,next-state))))))))
   (state-pass : State (s) -> * ()
-    [(,symbol (,[assign-pass : assign] ...) ,[next-state-pass : next-state])
+    [(,symbol (,[assign] ...) ,[next-state])
      (list symbol assign next-state)])
   (operation-pass : Operation (o) -> * ()
-    [(,symbol (,[port-pass : port] ...) (,[state-pass : state] ...))
+    [(,symbol (,[port] ...) (,[state-pass : state] ...))
      (list symbol port state)])
-  (module-pass : Module (mo) -> * ()
-    [(,symbol (,[declaration-pass : declaration] ...) (,[operation-pass : operation] ...))
-     (list symbol declaration operation)]))
+  (module-pass : Module (mo) -> Module ()
+    [(,symbol (,[declaration] ...) (,[operation-pass : operation] ...))
+     (let ([operation-names (extract-operation-names operation)]
+           [ports (extract-ports operation)]
+           [assign-states (extract-assign-states operation)]
+           [next-states (extract-next-states operation)])
+       `(,symbol (,operation-names ...) (,ports ...) (,declaration ...) (,assign-states ...) (,next-states ...)))]))
 
-;; brainstorm:
-;; (pair
-;;  ((mem cars (8 . 0) (255 . 0))
-;;   (mem cdrs (8 . 0) (255 . 0))
-;;   (reg next_addr (8 . 0) (const 1'd1)))
-;;  (cons ((input car (8 . 0))
-;;         (input cdr (8 . 0))
-;;         (output pair_out (8 . 0)))
-;;        ((store-cons
-;;          (((mem cars) (reg next_addr) (reg car))
-;;           ((mem cdrs) (reg next_addr) (reg cdr))
-;;           ((reg next_addr) (op +) (reg next_addr) (const 1'd1))
-;;           ((output ref_out) (op ,) (const 1'b1) (reg next_addr))))))
-;;  (car ((input pair_in (8 . 0))
-;;        (output pair_out (8 . 0)))
-;;       ((load-car
-;;         (((output pair_out) (mem cars) (input pair_in (7 . 0)))))))
-;;  (cdr ((input pair_in (8 . 0))
-;;        (output pair_out (8 . 0)))
-;;       ((load-cdr
-;;         (((output pair_out) (mem cdrs) (input pair_in (7 . 0)))))))
-;;  (set_car ((input pair_in (8 . 0))
-;;            (input car (8 . 0)))
-;;           ((store-car
-;;             (((mem cars) (input pair_in (7 . 0)) (input car))))))
-;;  (set_cdr ((input pair_in (8 . 0))
-;;            (input cdr (8 . 0)))
-;;           ((store-cdr
-;;             (((mem cdrs) (input pair_in (7 . 0)) (input cdr)))))))
+;; (define-pass output-rtl : rtl-adt (ast) -> * ()
+;;   (register-pass : Register (r) -> * ()
+;;     [(reg ,symbol) (list 'reg symbol)]
+;;     [(reg ,symbol ,size) (list 'reg symbol size)])
+;;   (constant-pass : Constant (c) -> * ()
+;;     [(const ,bitwidth ,baseident ,literal) (list bitwidth baseident literal)])
+;;   (input-pass : Input (i) -> * ()
+;;     [(in ,symbol) (list 'in symbol)]
+;;     [(in ,symbol ,size) (list 'in symbol size)])
+;;   (output-pass : Output (i) -> * ()
+;;     [(out ,symbol) (list 'out symbol)]
+;;     [(out ,symbol ,size) (list 'out symbol size)])
+;;   (port-pass : Port (p) -> * ())
+;;   (memory-ref-pass : MemoryRef (mr) -> * ())
+;;   (memory-pass : Memory (m) -> * ()
+;;     [(mem ,symbol ,[memory-ref-pass : memory-ref]) (list 'mem symbol memory-ref)])
+;;   (memory-decl-pass : MemoryDecl (md) -> * ()
+;;     [(mem ,symbol ,size0 ,size1) (list 'mem symbol size0 size1)])
+;;   (declaration-pass : Declaration (d) -> * ())
+;;   (target-pass : Target (t) -> * ())
+;;   (value-pass : Value (v) -> * ())
+;;   (unary-op-pass : UnaryOp (uo) -> * ()
+;;     [(op ,unop) (list 'op unop)])
+;;   (binary-op-pass : BinaryOp (bo) -> * ()
+;;     [(op ,binop) (list 'op binop)])
+;;   (assign-pass : Assign (a) -> * ()
+;;     [(,[target-pass : target] ,[value-pass : value])
+;;      (list target value)]
+;;     [(,[target-pass : target] ,[unary-op-pass : unary-op] ,[value-pass : value])
+;;      (list target unary-op value)]
+;;     [(,[target-pass : target] ,[value-pass : value1] ,[binary-op-pass : binary-op] ,[value-pass : value2])
+;;      (list target value1 binary-op value2)])
+;;   (case-statement-pass : CaseStatement (cs) -> * ()
+;;     [(case ,[value-pass : value0] (,[value-pass : value1] ,symbol1) ... ,symbol0)
+;;      (list 'case value0 (map cons value1 symbol1) symbol0)])
+;;   (next-state-pass : NextState (ns) -> * ()
+;;     [,symbol symbol])
+;;   (state-pass : State (s) -> * ()
+;;     [(,symbol (,[assign-pass : assign] ...) ,[next-state-pass : next-state])
+;;      (list symbol assign next-state)])
+;;   (operation-pass : Operation (o) -> * ()
+;;     [(,symbol (,[port-pass : port] ...) (,[state-pass : state] ...))
+;;      (list symbol port state)])
+;;   (module-pass : Module (mo) -> * ()
+;;     [(,symbol (,[declaration-pass : declaration] ...) (,[operation-pass : operation] ...))
+;;      (list symbol declaration operation)]))
