a27b342d6ad9697ca15c32dba7ea8acd02ac1273
mikeurbach <mikeurbach@gmail.com>
2019-12-07 11:01:34 -0800

    update notes, start working through full suite of tests

--- nanopass/rtl.rkt
+++ nanopass/rtl.rkt
@@ -4,10 +4,7 @@
 
 (provide
  rtl-adt
- adt-to-fsm
- add-boilerplate
- add-default-assigns
- preprint-to-pprint)
+ adt-to-verilog)
 
 (define (size? e)
   (and (pair? e)
@@ -627,49 +624,10 @@
                 (text "end"))))
       (text "endmodule"))]))
 
-;; (define-pass output-rtl : rtl-adt (ast) -> * ()
-;;   (register-pass : Register (r) -> * ()
-;;     [(reg ,symbol) (list 'reg symbol)]
-;;     [(reg ,symbol ,size) (list 'reg symbol size)])
-;;   (constant-pass : Constant (c) -> * ()
-;;     [(const ,bitwidth ,baseident ,literal) (list bitwidth baseident literal)])
-;;   (input-pass : Input (i) -> * ()
-;;     [(in ,symbol) (list 'in symbol)]
-;;     [(in ,symbol ,size) (list 'in symbol size)])
-;;   (output-pass : Output (i) -> * ()
-;;     [(out ,symbol) (list 'out symbol)]
-;;     [(out ,symbol ,size) (list 'out symbol size)])
-;;   (port-pass : Port (p) -> * ())
-;;   (memory-ref-pass : MemoryRef (mr) -> * ())
-;;   (memory-pass : Memory (m) -> * ()
-;;     [(mem ,symbol ,[memory-ref-pass : memory-ref]) (list 'mem symbol memory-ref)])
-;;   (memory-decl-pass : MemoryDecl (md) -> * ()
-;;     [(mem ,symbol ,size0 ,size1) (list 'mem symbol size0 size1)])
-;;   (declaration-pass : Declaration (d) -> * ())
-;;   (target-pass : Target (t) -> * ())
-;;   (value-pass : Value (v) -> * ())
-;;   (unary-op-pass : UnaryOp (uo) -> * ()
-;;     [(op ,unop) (list 'op unop)])
-;;   (binary-op-pass : BinaryOp (bo) -> * ()
-;;     [(op ,binop) (list 'op binop)])
-;;   (assign-pass : Assign (a) -> * ()
-;;     [(,[target-pass : target] ,[value-pass : value])
-;;      (list target value)]
-;;     [(,[target-pass : target] ,[unary-op-pass : unary-op] ,[value-pass : value])
-;;      (list target unary-op value)]
-;;     [(,[target-pass : target] ,[value-pass : value1] ,[binary-op-pass : binary-op] ,[value-pass : value2])
-;;      (list target value1 binary-op value2)])
-;;   (case-statement-pass : CaseStatement (cs) -> * ()
-;;     [(case ,[value-pass : value0] (,[value-pass : value1] ,symbol1) ... ,symbol0)
-;;      (list 'case value0 (map cons value1 symbol1) symbol0)])
-;;   (next-state-pass : NextState (ns) -> * ()
-;;     [,symbol symbol])
-;;   (state-pass : State (s) -> * ()
-;;     [(,symbol (,[assign-pass : assign] ...) ,[next-state-pass : next-state])
-;;      (list symbol assign next-state)])
-;;   (operation-pass : Operation (o) -> * ()
-;;     [(,symbol (,[port-pass : port] ...) (,[state-pass : state] ...))
-;;      (list symbol port state)])
-;;   (module-pass : Module (mo) -> * ()
-;;     [(,symbol (,[declaration-pass : declaration] ...) (,[operation-pass : operation] ...))
-;;      (list symbol declaration operation)]))
+(define adt-to-verilog
+  (compose1
+   pretty-print
+   preprint-to-pprint
+   add-default-assigns
+   add-boilerplate
+   adt-to-fsm))
