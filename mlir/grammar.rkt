9b29eebaa4127584c18a0e5da89ad19981317be3
mikeurbach <mikeurbach@gmail.com>
2020-03-10 20:49:59 -0600

    add parsing support for the rest of the identifiers

--- mlir/grammar.rkt
+++ mlir/grammar.rkt
@@ -1,12 +1,19 @@
 #lang brag
 
-top: integer-literal | float-literal | string-literal | bare-id | bare-id-list
+top: integer-literal | float-literal | string-literal | bare-id | bare-id-list | suffix-id | ssa-id | ssa-id-list | symbol-ref-id | caret-id
 
+# literals
 decimal-literal: DECIMAL_LITERAL
 hexadecimal-literal: HEXADECIMAL_LITERAL
 integer-literal: decimal-literal | hexadecimal-literal
 float-literal: FLOAT_LITERAL
 string-literal: STRING_LITERAL
 
+# identifiers
 bare-id: BARE_ID
-bare-id-list: bare-id [/COMMA bare-id]
+bare-id-list: bare-id [/COMMA bare-id]*
+suffix-id: SUFFIX_ID
+ssa-id: /PERCENT suffix-id
+ssa-id-list: ssa-id [/COMMA ssa-id]*
+symbol-ref-id: /AMPERSAND (suffix-id | string-literal)
+caret-id: /CARET suffix-id
--- mlir/lexer.rkt
+++ mlir/lexer.rkt
@@ -8,6 +8,13 @@
   (define mlir-lexer
     (lexer-src-pos
      ["," (token 'COMMA lexeme)]
+     ["%" (token 'PERCENT lexeme)]
+     ["@" (token 'AMPERSAND lexeme)]
+     ["^" (token 'CARET lexeme)]
+     [(:or (:+ numeric) (:: (:or "$" "." "_" "-" alphabetic) (:* (:or "$" "." "_" "-" numeric alphabetic))))
+      (token 'SUFFIX_ID lexeme)]
+     [(:: (:or "_" alphabetic) (:* (:or "_" "$" "." numeric alphabetic)))
+      (token 'BARE_ID lexeme)]
      [(:+ numeric)
       (token 'DECIMAL_LITERAL (string->number lexeme))]
      [(:: "0x" (:+ (:or numeric (char-range "a" "f") (char-range "A" "F"))))
@@ -15,8 +22,6 @@
      [(:: (:? (:or "+" "-")) (:+ numeric) (:? ".") (:* numeric) (:? (:or "e" "E") (:? (:or "+" "-")) (:+ numeric)))
       (token 'FLOAT_LITERAL (string->number lexeme))]
      [(:: "\"" (:* (:~ "\"" "\n" "\f" "\v" "\r"))  "\"")
-      (token 'STRING_LITERAL (substring lexeme 1 (sub1 (string-length lexeme))))]
-     [(:: (:or "_" alphabetic) (:* (:or "_" "$" "." numeric alphabetic)))
-      (token 'BARE_ID lexeme)]))
+      (token 'STRING_LITERAL (substring lexeme 1 (sub1 (string-length lexeme))))]))
   (define (next-token) (mlir-lexer input))
   next-token)
